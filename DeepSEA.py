015b916ff1ec612d977d858909fc2e5c1fa7871c
tiagocabralborelli <tiago.cabral422@gmail.com>
2024-09-03 12:48:28 -0300

    [Feat] Multi output added

--- DeepSEA.py
+++ DeepSEA.py
@@ -16,6 +16,8 @@ def download():
         "Models directory already exists"
     print("Downloading model T800...")
     os.system("curl https://zenodo.org/records/11086187/files/NCRD-unalign.zip?download=1 --output models/NCRD-unalign.zip")
+
+
     print("Download complete")
     print("Installing model T800...")
     os.system("unzip models/NCRD-unalign.zip -d models")
@@ -24,37 +26,42 @@ def download():
 
 @deepsea.command()
 @click.option("--input",  help = "Path to input fasta file")
-@click.option("--output", help = "Output file name")
+@click.option("--out", help = "Output file name")
 @click.option("--model",  help = "Number of models to annotate ARP. Options: 1 or 2]")
 
-def predict(input, output, model):
+def predict(input, out, model):
     #MODELS
 
-    model1 = model_loader()
-    model2 = model_loader(
+    model = int(model)
+
+    model1 = model_loader("models/cnn-same-aligned")
+    model2 = model_loader("models/cnn-unaligned")
 
     ENCODER = enc_loader()
 
     data = load_fasta(input = input)
+
     model1_yhat = apply_model(model1,data,ENCODER)
     model2_yhat = apply_model(model2,data,ENCODER)
 
     if model == 1:
-        data["Model1-class"] = model1_yhat[0]
-        data["Model-prob"] = model1_yhat[1]
+        data["model1-class"] = model1_yhat[0]
+        data["model1-prob"] = model1_yhat[1]
 
     elif model == 2:
-        data["Model1-class"] = model1_yhat[0]
-        data["Model1-prob"] = model1_yhat[1]
-        data["Model2-class"] = model2_yhat[0]
-        data["Model2-prob"] = model2_yhat[1]
+        data["model1-class"] = model1_yhat[0]
+        data["model1-prob"] = model1_yhat[1]
+        data["model2-class"] = model2_yhat[0]
+        data["model2-prob"] = model2_yhat[1]
 
     try:
         os.makedirs("DeepSEA-output")
     except:
         print("Output file already exists!")
     data["sequence"] = data.sequence.apply(lambda x: "".join(x.split(" ")) )
-    data.to_csv(f"DeepSEA-output/{output}.csv", index = False)
+    
+    print(data[["identifier","model1-class","model1-prob","model2-class","model2-prob","sequence"]])
+    data[["identifier","model1-class","model1-prob","model2-class","model2-prob","sequence"]].to_csv(f"DeepSEA-output/{out}.csv", index = False)
 
 
 if __name__ == '__main__':
